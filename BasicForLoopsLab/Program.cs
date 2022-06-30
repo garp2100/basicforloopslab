219c7a5859c017769cd7961617ad75e224f79ce9
Gustavo Rivera <garp2100@gmail.com>
2022-06-29 23:10:27 -0400

    second commit

--- BasicForLoopsLab/Program.cs
+++ BasicForLoopsLab/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Prompt the user to enter a number.  Use a for-loop to calculate the sum of all the numbers from 1 to the number entered.
+while (true)
+{
+    int j;
+    int sum = 0;
+    Console.WriteLine("Please enter a number: ");
+    int input = int.Parse(Console.ReadLine());
+    for (j = 1; j <= input; j++)
+    {
+        sum = sum + j;
+        
+    }
+    Console.WriteLine(sum);
+    while (true)
+    {
+        Console.WriteLine("Would you like to continue? (y/n)?");
+        string restartValue = Console.ReadLine().ToLower();
+        if (restartValue == "y")
+            break;
+        else Console.WriteLine("Goodbye!");
+        Environment.Exit(0);
+    }
+}
